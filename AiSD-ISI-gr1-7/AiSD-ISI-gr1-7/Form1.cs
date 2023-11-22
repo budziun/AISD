e1621bef04ec33374531556a8ce5d2e27e734a74
Jakub Budzich <115219579+budziun@users.noreply.github.com>
2023-11-22 11:03:02 +0100

    Add files via upload

---
+++ AiSD-ISI-gr1-7/AiSD-ISI-gr1-7/Form1.cs
@@ -0,0 +1,15 @@
+namespace AiSD_ISI_gr1_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
